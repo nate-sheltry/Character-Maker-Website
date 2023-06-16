3c63fc0873408d117697fbfb79bacd1a234c146e
Nathaniel Dunham <dun19007@byui.edu>
2023-06-16 02:35:27 -0700

    undid some "optimizations"

--- Fallout Tools/critter-script.js
+++ Fallout Tools/critter-script.js
@@ -7,7 +7,7 @@ const __critterResults = document.querySelector("#critter-results")
 
 const options = {
     root: document.querySelector("#scrollArea"),
-    rootMargin: "0px",
+    rootMargin: "200px",
     threshold: 0,
 }
 
@@ -26,53 +26,51 @@ function populateList(){
     })
 }
 
-function populateSpecial(value, resultCard){
-    const specialStats = resultCard.querySelector(".special-stats").querySelectorAll('.special-stat')    
-    for(let i = 0; i < specialStats.length; i++){
-        let child = specialStats[i];
-        let valueKeys = Object.keys(value.special)
-        child.children[0].textContent = `${valueKeys[i]}:`
-        child.children[1].textContent = value.special[valueKeys[i]]
-    };
-}
-function populateDT(value, resultCard){
-    const damageThresholds = resultCard.querySelector(".damage-thresholds").querySelectorAll('.damage-threshold')
-    for(let i = 0; i < damageThresholds.length; i++){
-        let child = damageThresholds[i];
-        let valueKeys = Object.keys(value.damageThreshold)
-        child.children[0].textContent = `${valueKeys[i].replace(valueKeys[i][0], valueKeys[i][0].toUpperCase()).replace("_", ".")}:`
-        child.children[1].textContent = value.damageThreshold[valueKeys[i]]
-    };
-}
-function populateSecStats(value, resultCard){
-    const secondaryStats = resultCard.querySelector(".secondary-stats").querySelectorAll('.secondary-stat')
-    for(let i = 0; i < secondaryStats.length; i++){
-        let child = secondaryStats[i];
-        let valueKeys = Object.keys(value.secondaryStats)
-        child.children[0].textContent = `${valueKeys[i]}:`
-        child.children[1].textContent = value.secondaryStats[valueKeys[i]]
-    };
-}
-function populateSkills(value, resultCard){
-    const skills = resultCard.querySelector(".skills").querySelectorAll('.skill')
-    for(let i = 0; i < skills.length; i++){
-        let child = skills[i];
-        let valueKeys = Object.keys(value.skillsInfo)
-        child.children[0].textContent = `${valueKeys[i].replace("_", ".").replace("-", " ").replace("_", ".")
-        .replace(valueKeys[i][0], valueKeys[i][0].toUpperCase())}:`
-        child.children[1].textContent = value.skillsInfo[valueKeys[i]]
-    };
-
-}
-function populateGmInfo(value, resultCard){
-    const gmInfo = resultCard.querySelector(".gminfo-stats").querySelectorAll('.gminfo-stat')
-    for(let i = 0; i < gmInfo.length; i++){
-        let child = gmInfo[i];
-        let valueKeys = Object.keys(value.gmInfo)
-        child.children[0].textContent = `${valueKeys[i].replace("_", ".").replace("-", " ").replace("_", ".")
-        .replace(valueKeys[i][0], valueKeys[i][0].toUpperCase())}:`
-        child.children[1].textContent = value.gmInfo[valueKeys[i]]
-    };
+function populateItems(container){
+    let item = dataItems.find(obj => obj.element === container)
+        if(item.element === container){
+            fetch(dataIndex[item.name].url).then((res) => res.json()).then(value => {
+            const resultCard = container
+            const specialStats = resultCard.querySelector(".special-stats").querySelectorAll('.special-stat')
+            const secondaryStats = resultCard.querySelector(".secondary-stats").querySelectorAll('.secondary-stat')
+            const gmInfo = resultCard.querySelector(".gminfo-stats").querySelectorAll('.gminfo-stat')
+            const damageThresholds = resultCard.querySelector(".damage-thresholds").querySelectorAll('.damage-threshold')
+            const skills = resultCard.querySelector(".skills").querySelectorAll('.skill')
+            
+            for(let i = 0; i < specialStats.length; i++){
+                let child = specialStats[i];
+                let valueKeys = Object.keys(value.special)
+                child.children[0].textContent = `${valueKeys[i]}:`
+                child.children[1].textContent = value.special[valueKeys[i]]
+            };
+            for(let i = 0; i < damageThresholds.length; i++){
+                let child = damageThresholds[i];
+                let valueKeys = Object.keys(value.damageThreshold)
+                child.children[0].textContent = `${valueKeys[i].replace(valueKeys[i][0], valueKeys[i][0].toUpperCase()).replace("_", ".")}:`
+                child.children[1].textContent = value.damageThreshold[valueKeys[i]]
+            };
+            for(let i = 0; i < secondaryStats.length; i++){
+                let child = secondaryStats[i];
+                let valueKeys = Object.keys(value.secondaryStats)
+                child.children[0].textContent = `${valueKeys[i]}:`
+                child.children[1].textContent = value.secondaryStats[valueKeys[i]]
+            };
+            for(let i = 0; i < skills.length; i++){
+                let child = skills[i];
+                let valueKeys = Object.keys(value.skillsInfo)
+                child.children[0].textContent = `${valueKeys[i].replace("_", ".").replace("-", " ").replace("_", ".")
+                .replace(valueKeys[i][0], valueKeys[i][0].toUpperCase())}:`
+                child.children[1].textContent = value.skillsInfo[valueKeys[i]]
+            };
+            for(let i = 0; i < gmInfo.length; i++){
+                let child = gmInfo[i];
+                let valueKeys = Object.keys(value.gmInfo)
+                child.children[0].textContent = `${valueKeys[i].replace("_", ".").replace("-", " ").replace("_", ".")
+                .replace(valueKeys[i][0], valueKeys[i][0].toUpperCase())}:`
+                child.children[1].textContent = value.gmInfo[valueKeys[i]]
+            };
+        });
+    }
 }
 
 __critterSearch.addEventListener('keypress', e => {
@@ -100,21 +98,11 @@ __critterSearch.addEventListener('keypress', e => {
 populateList();
 
 __critterResults.querySelectorAll(".critter-result").forEach(child => {
-    const observer = new IntersectionObserver(async(container, containerObserver) => {
+    const observer = new IntersectionObserver((container, containerObserver) => {
         if(!container[0].isIntersecting){
             return;
         }
-        let item = dataItems.find(obj => obj.element === container[0].target)
-        if(item.element === container[0].target){
-            fetch(dataIndex[item.name].url).then((res) => res.json()).then(value => {
-                const resultCard = container[0].target
-                populateSpecial(value, resultCard)
-                populateDT(value, resultCard)
-                populateSecStats(value, resultCard)
-                populateSkills(value, resultCard)
-                populateGmInfo(value, resultCard)
-            })
-        }
+        populateItems(container[0].target);
         containerObserver.unobserve(container[0].target)
     }, options)
     observer.observe(child)
