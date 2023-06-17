d9cba7574c0f9195a582ffeb3ca0d8e0ee4ff7f8
Nathaniel Dunham <dun19007@byui.edu>
2023-06-16 23:51:07 -0700

    added a basic div to cut down on CSS load time.

--- Fallout Tools/critter-script.js
+++ Fallout Tools/critter-script.js
@@ -1,5 +1,6 @@
 
 let dataItems = []
+let load = false;
 
 const __critterSearch = document.querySelector("#critter-search")
 const __critterTemplate = document.querySelector("[data-attribute=\"critter-template\"]");
@@ -7,7 +8,7 @@ const __critterResults = document.querySelector("#critter-results")
 
 const options = {
     root: document.querySelector("#scrollArea"),
-    rootMargin: "200px",
+    rootMargin: "100%",
     threshold: 0,
 }
 
@@ -17,65 +18,69 @@ const dataIndex = await fetch(apiLink).then((res) => {return res.json()});
 
 function populateList(){
     dataItems = Object.keys(dataIndex).map(key => {
-        const resultCard = __critterTemplate.content.cloneNode(true).children[0]
-        const head = resultCard.querySelector(".head")
+        const resultCard = document.createElement('div')
+        resultCard.classList.toggle('critter-container', true);
+        resultCard.textContent = 'Loading'
 
-        head.textContent = `Name: ${dataIndex[key].name}`
         __critterResults.append(resultCard);
         return {name: key, element: resultCard}
     })
 }
 
-function populateItems(container){
+async function populateItems(container){
+    console.log(container)
     let item = dataItems.find(obj => obj.element === container)
-        if(item.element === container){
-            fetch(dataIndex[item.name].url).then((res) => res.json()).then(value => {
-            const resultCard = container
+    if(item.element === container){
+            const critterTemplate = __critterTemplate.content.cloneNode(true).children[0]
+            const value = await fetch(dataIndex[item.name].url).then((res) => {return res.json()});
+            const resultCard = critterTemplate
+            const head = resultCard.querySelector(".head")
+            head.textContent = `Name: ${dataIndex[item.name].name}`
             const specialStats = resultCard.querySelector(".special-stats").querySelectorAll('.special-stat')
             const secondaryStats = resultCard.querySelector(".secondary-stats").querySelectorAll('.secondary-stat')
             const gmInfo = resultCard.querySelector(".gminfo-stats").querySelectorAll('.gminfo-stat')
             const damageThresholds = resultCard.querySelector(".damage-thresholds").querySelectorAll('.damage-threshold')
             const skills = resultCard.querySelector(".skills").querySelectorAll('.skill')
             
-            for(let i = 0; i < specialStats.length; i++){
-                let child = specialStats[i];
+            specialStats.forEach((child, i = 0) => {
                 let valueKeys = Object.keys(value.special)
                 child.children[0].textContent = `${valueKeys[i]}:`
                 child.children[1].textContent = value.special[valueKeys[i]]
-            };
-            for(let i = 0; i < damageThresholds.length; i++){
-                let child = damageThresholds[i];
+                i++;
+            });
+            damageThresholds.forEach((child, i = 0) => {
                 let valueKeys = Object.keys(value.damageThreshold)
                 child.children[0].textContent = `${valueKeys[i].replace(valueKeys[i][0], valueKeys[i][0].toUpperCase()).replace("_", ".")}:`
                 child.children[1].textContent = value.damageThreshold[valueKeys[i]]
-            };
-            for(let i = 0; i < secondaryStats.length; i++){
-                let child = secondaryStats[i];
+                i++;
+            });
+            secondaryStats.forEach((child, i = 0) => {
                 let valueKeys = Object.keys(value.secondaryStats)
                 child.children[0].textContent = `${valueKeys[i]}:`
                 child.children[1].textContent = value.secondaryStats[valueKeys[i]]
-            };
-            for(let i = 0; i < skills.length; i++){
-                let child = skills[i];
+                i++;
+            });
+            skills.forEach((child, i = 0) => {
                 let valueKeys = Object.keys(value.skillsInfo)
                 child.children[0].textContent = `${valueKeys[i].replace("_", ".").replace("-", " ").replace("_", ".")
                 .replace(valueKeys[i][0], valueKeys[i][0].toUpperCase())}:`
                 child.children[1].textContent = value.skillsInfo[valueKeys[i]]
-            };
-            for(let i = 0; i < gmInfo.length; i++){
-                let child = gmInfo[i];
+                i++;
+            });
+            gmInfo.forEach((child, i = 0) => {
                 let valueKeys = Object.keys(value.gmInfo)
                 child.children[0].textContent = `${valueKeys[i].replace("_", ".").replace("-", " ").replace("_", ".")
                 .replace(valueKeys[i][0], valueKeys[i][0].toUpperCase())}:`
                 child.children[1].textContent = value.gmInfo[valueKeys[i]]
-            };
-        });
+                i++;
+            });
+        container.append(critterTemplate)
+        container.width = 'auto'
+        container.height = 'auto'
     }
 }
 
-__critterSearch.addEventListener('keypress', e => {
-    if(e.keyCode != 13)
-        return
+__critterSearch.addEventListener('input', e => {
     const value = e.target.value;
     if(value.length > 1){
         
@@ -97,13 +102,28 @@ __critterSearch.addEventListener('keypress', e => {
 
 populateList();
 
-__critterResults.querySelectorAll(".critter-result").forEach(child => {
+let critterObserver = [];
+
+__critterResults.querySelectorAll(".critter-container").forEach(child => {
     const observer = new IntersectionObserver((container, containerObserver) => {
         if(!container[0].isIntersecting){
             return;
         }
+        console.log('is being observed')
         populateItems(container[0].target);
+        container[0].target.textContent = '';
         containerObserver.unobserve(container[0].target)
     }, options)
     observer.observe(child)
+    critterObserver.push(observer);
+})
+
+document.querySelector("#load-button").addEventListener("click", e => {
+    __critterResults.querySelectorAll(".critter-container").forEach(child => {
+        populateItems(child)
+        critterObserver.forEach(ob => {
+            ob.unobserve(child)
+        })
+        
+    })
 })
